85e2f8965ae346f49de0584275104e75c192e63c
Anne Starley <annestarley@gmail.com>
2018-02-10 16:10:24 -0800

    adding javascript to cat page

---
+++ scripts/cats.js
@@ -0,0 +1,16 @@
+let catBoxes = document.querySelectorAll('.cat-boxes')
+console.log(catBoxes[0])
+
+catBoxes.forEach(box => {
+  box.addEventListener('click', (e) => {
+    e.style.border = 'solid 5px white'
+  })
+})
+
+
+var imgs = ["./images/gumpy-cat/2.jpg", "./images/gumpy-cat/3.jpg", "./images/gumpy-cat/4.jpg"];
+
+function changeImage() {
+    document.getElementById("grumpy-cat").src = imgs[0];
+    imgs.push(imgs.shift())
+}
