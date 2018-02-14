e374f2a154d374b66a6fd0b9c04e5605a89baba7
Anne Starley <annestarley@gmail.com>
2018-02-14 15:12:14 -0800

    added information for get user by id

--- scripts/user.js
+++ scripts/user.js
@@ -1,6 +1,17 @@
+const baseURL = 'http://localhost:3000'
+let userId = localStorage.getItem('id')
+console.log(userId);
+
 let username = localStorage.getItem('username')
 let greeting = document.querySelector('#greeting')
 let h2 = document.createElement('h2')
 h2.innerText = `Hello ${username}!`
 
 greeting.prepend(h2)
+
+axios.get(`${baseURL}/users/${userId}`)
+  .then(userResults => {
+    let profilePic = document.createElement('img')
+    profilePic.setAttribute('src', `${userResults.data[0].image}`)
+    console.log(userResults.data[0].image);
+  })
