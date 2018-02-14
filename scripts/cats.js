228db2b328c3082ab3f0f8e184002c2c7e48933a
Anne Starley <annestarley@gmail.com>
2018-02-14 11:54:36 -0800

    hooked to meow page

--- scripts/cats.js
+++ scripts/cats.js
@@ -1,16 +1,7 @@
-// let catBoxes = document.querySelectorAll('.cat-boxes')
-// console.log(catBoxes[0])
-//
-// catBoxes.forEach(box => {
-//   box.addEventListener('click', (e) => {
-//     e.style.border = 'solid 5px white'
-//   })
-// })
-//
-//
-// var imgs = ["./images/gumpy-cat/2.jpg", "./images/gumpy-cat/3.jpg", "./images/gumpy-cat/4.jpg"];
-//
-// function changeImage() {
-//     document.getElementById("grumpy-cat").src = imgs[0];
-//     imgs.push(imgs.shift())
-// }
+const baseURL = 'http://localhost:3000'
+
+
+axios.get(`${baseURL}/cats`)
+  .then(results => {
+    console.log(results);
+  })
